0308e6ad29a6f1c05f91e029c9e8f22e860f9f36
GitHub Action <action@github.com>
2022-07-05 23:17:55 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "macos_ui_intellij"
---
+++ src/main/kotlin/com/github/groovinchip/macosuiintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.groovinchip.macosuiintellij.services
+
+import com.github.groovinchip.macosuiintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
